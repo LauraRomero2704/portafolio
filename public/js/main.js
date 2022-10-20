c39b596a76765929c866cf9febfeca1fe4bc44c4
Harold Levid <hlmarin2003@gmail.com>
2022-10-20 09:54:59 -0500

    Portafolio index y error 404

--- public/js/main.js
+++ public/js/main.js
@@ -1,59 +1,39 @@
-import 'animate.css'
-
-// Menu Responsive
 let menuVisible = false;
-
-// Función que oculta o muestra el menu
-function mostrarOcultarMenu()
-{
-    if(menuVisible)
-    {
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
         document.getElementById("nav").classList ="";
         menuVisible = false;
-    }
-    
-    else
-    {
+    }else{
         document.getElementById("nav").classList ="responsive";
         menuVisible = true;
     }
-} 
+}
 
-window.addEventListener("scroll", function()
-{
-    var header = this.document.querySelector("header");
-    header.classList.toggle("abajo", window.scrollY>0);
-})
-
-
-// Seccion Habilidades
-function seleccionar()
-{
+function seleccionar(){
     //oculto el menu una vez que selecciono una opcion
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
-
 //Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades()
-{
+function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300)
-    {
+    if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
         habilidades[2].classList.add("java");
-        habilidades[3].classList.add("comunicacion");
-        habilidades[4].classList.add("trabajo");
-        habilidades[5].classList.add("creatividad");
-        habilidades[6].classList.add("dedicacion");
+        habilidades[3].classList.add("mysql");
+        habilidades[4].classList.add("comunicacion");
+        habilidades[5].classList.add("trabajo");
+        habilidades[6].classList.add("creatividad");
+        habilidades[7].classList.add("dedicacion");
     }
 }
 
+
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function()
-{
+window.onscroll = function(){
     efectoHabilidades();
-}
+} 
